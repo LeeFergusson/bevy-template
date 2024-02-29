8645cd3a175818fdb03028a45a79e1bc43be0ff9
Lee Fergusson <lee.fergusson@googlemail.com>
2024-02-29 22:28:13 +0000

    Fix dependencies

--- default.nix
+++ default.nix
@@ -1,8 +1,15 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-  name = "dev-environment";
-  buildInputs = [
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell rec {
+  nativeBuildInputs = [
     pkg-config
-    # zlib
   ];
+  buildInputs = [
+    udev alsa-lib vulkan-loader
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+    libxkbcommon wayland # To use the wayland feature
+  ];
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
