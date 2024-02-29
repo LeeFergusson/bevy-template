ede7d1ad32ce1fe9ec383bce181755c3ce8d5b42
Lee Fergusson <lee.fergusson@googlemail.com>
2024-02-29 23:47:51 +0000

    Improve nix compatability

--- default.nix
+++ default.nix
@@ -1,15 +1,8 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-with pkgs;
-
-mkShell rec {
-  nativeBuildInputs = [
-    pkg-config
-  ];
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "dev-environment";
   buildInputs = [
-    udev alsa-lib vulkan-loader
-    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
-    libxkbcommon wayland # To use the wayland feature
+    pkg-config
+    # zlib
   ];
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
