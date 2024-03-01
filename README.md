fc7d31ea39f76540ac51bae0a1ee86279e105df6
Lee Fergusson <lee.fergusson@googlemail.com>
2024-03-01 13:55:51 +0000

    Improve template

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Rust - Bevy Template
 
+![](https://bevyengine.org/assets/bevy_logo_dark.svg)
+
 Requirements: [Link](https://github.com/bevyengine/bevy/blob/main/docs/linux_dependencies.md) 
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
-    .add_systems(Startup, setup)
-    .add_systems(Update, hello_world)
+    .add_systems(Startup, setup_camera)
+    .add_systems(Startup, spawn_logo)
     .run();
 }
 
-pub fn setup(mut commands: Commands) {
-  commands.spawn(Camera2dBundle::default());
+pub fn setup_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+  let window = window_query.get_single().unwrap();
+
+  commands.spawn(Camera2dBundle {
+    transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+    ..Default::default()
+  });
 }
 
-fn hello_world() {
-  println!("Hello, world!");
+#[derive(Component)]
+struct Logo {}
+
+fn spawn_logo(
+  mut commands: Commands,
+  window_query: Query<&Window, With<PrimaryWindow>>,
+  asset_server: Res<AssetServer>,
+) {
+  let window = window_query.get_single().unwrap();
+
+  commands.spawn((
+    SpriteBundle {
+      transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+      texture: asset_server.load("sprites/bevy_logo_dark.png"),
+      ..Default::default()
+    },
+    Logo {},
+  ));
 }
